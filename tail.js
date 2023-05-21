3047f1e60785ab01f05ee823f898f0aafb93419d
xweetx <xweet_xilenze@yahoo.com>
2023-05-21 00:43:34 -0600

    changes tail

--- tail.js
+++ tail.js
@@ -1,22 +1,22 @@
-const assertEqual = function (a,b) {
-  if (a === b) {
-    console.log(`✅✅✅Assertion Passed:  ${a}  ===   ${b}`);
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑Assertion Failed:  ${a}  !==  ${b}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-//array to return back to assert equal
 const tail = function(array) {
-  
   if (array.length <= 1) {
     return [];
-
   }
   return array.slice(1);
 };
 
-//test code
-assertEqual(tail([1, 2, 3]).toString(), [2, 3].toString());  // to check if the same
-assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).toString(), ["Lighthouse", "Labs"].toString()); //Does not include the head part
+
+assertEqual(tail([1, 2, 3]).toString(), [2, 3].toString());
+assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).toString(), ["Lighthouse", "Labs"].toString());
+
+
+
 
