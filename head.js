2ca442bcd83f06c63dcf7233cdcf0c8bc1ff29ba
xweetx <xweet_xilenze@yahoo.com>
2023-05-21 00:34:52 -0600

    changes heads

--- head.js
+++ head.js
@@ -1,18 +1,18 @@
 //head function implementation
 
-const assertEqual = function (a,b) {
-  if (a === b){
-    console.log('✅✅✅Assertion Passed: '+ [a] + ' === ' + [b]);
-    } else {
-      console.log('🛑🛑🛑Assertion Failed:' + [a] + ' !==' + [b]);  
-    }
+const assertEqual = function(a,b) {
+  if (a === b) {
+    console.log('✅✅✅Assertion Passed: ' + [a] + ' === ' + [b]);
+  } else {
+    console.log('🛑🛑🛑Assertion Failed:' + [a] + ' !==' + [b]);
+  }
 };
 
 const head = function(array) {
   if (array.length === 0) {
     return undefined;
   }
-   return array[0];
+  return array[0];
 };
 
 //test code
