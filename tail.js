3b54404b0cedc31edc43343f71d51e6bf42c8eba
xweetx <xweet_xilenze@yahoo.com>
2023-06-11 12:15:34 -0600

    tail updates

--- tail.js
+++ tail.js
@@ -1,10 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   if (array.length <= 1) {
@@ -17,6 +18,6 @@ const tail = function(array) {
 assertEqual(tail([1, 2, 3]).toString(), [2, 3].toString());
 assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).toString(), ["Lighthouse", "Labs"].toString());
 
-
+module.exports = tail;
 
 
