1d51c889f6d708704649c3df1a336367c1beae0b
xweetx <xweet_xilenze@yahoo.com>
2023-06-11 12:08:40 -0600

    updated

--- head.js
+++ head.js
@@ -20,3 +20,4 @@ const head = function(array) {
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
+module.exports = head;
---
+++ test/asserEqualTest.js
@@ -0,0 +1,6 @@
+// test/assertEqualTest.js
+
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+// ...
