d143b78315f2735e3d2e117e302bd5b1be5766f8
xweetx <xweet_xilenze@yahoo.com>
2023-06-11 12:28:04 -0600

    assertArraysEqual test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,20 @@
 //Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    return false;
-  }
+// const eqArrays = function(actual, expected) {
+//   if (actual.length !== expected.length) {
+//     return false;
+//   }
 
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
+//   for (let i = 0; i < actual.length; i++) {
+//     if (actual[i] !== expected[i]) {
+//       return false;
+//     }
+//   }
 
-  return true;
-};
+//   return true;
+// };
+
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -27,3 +29,5 @@ assertArraysEqual([1, 2, 3], [1, 2, 3]); // Pass
 assertArraysEqual([1, 2, 3], [3, 2, 1]); // Fail
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // Pass
 assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // Fail
+
+module.exports = assertArraysEqual;
