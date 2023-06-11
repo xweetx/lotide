b33d9cc5a781a38c54f52b012d33d84978c25361
xweetx <xweet_xilenze@yahoo.com>
2023-06-11 12:22:36 -0600

    eqArrays test

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,13 @@
 //Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 
-const assertEqual = function(actual,expected) {
-  if (actual === expected)
-    console.log('✅✅✅Assertion Passed: ' + [actual] + ' === ' + [expected]);
-  else if (actual !== expected)
-    console.log('🛑🛑🛑Assertion Failed:' + [actual] + ' !==' + [expected]);
-};
+// const assertEqual = function(actual,expected) {
+//   if (actual === expected)
+//     console.log('✅✅✅Assertion Passed: ' + [actual] + ' === ' + [expected]);
+//   else if (actual !== expected)
+//     console.log('🛑🛑🛑Assertion Failed:' + [actual] + ' !==' + [expected]);
+// };
+
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
@@ -26,3 +28,5 @@ eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,1 @@
+const eqArrays = require('../eqArrays');
