7ad10105fa6bfff6c399eefcd5dd1f5c20bedc54
xweetx <xweet_xilenze@yahoo.com>
2023-06-11 13:05:32 -0600

    middle Test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,1 +1,23 @@
-const middle = require('../middle');
+
+// const middle = require('../middle');
+
+const assert = require('chai').assert;
+const middle  = require('../middle');
+
+describe("#middle", () => {
+  it("returns [] for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
+
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns [2,3] for [1,2,3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+
+  it("returns [2] for [1,2,3] ", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+});
