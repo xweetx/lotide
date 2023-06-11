41a36c2c1c695501010ac6b918760fcc320c2189
xweetx <xweet_xilenze@yahoo.com>
2023-06-11 13:10:32 -0600

    tail test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,1 +1,18 @@
-const tail = require('../tail');
+// const tail = require('../tail');
+
+const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
+const tail = function(arr){
+    if (arr.length < 2) {
+        return arr;
+    } else {
+    return arr.slice(4);
+    }   
+}
+console.log(tail([1,2,3,4,5]))
+module.exports = tail;
+
+it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
