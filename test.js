bdb8f53dc8a45a4b6fb46bb9a8f25db1c9ba48fd
xweetx <xweet_xilenze@yahoo.com>
2023-06-13 16:03:18 -0600

    changes

---
+++ test.js
@@ -0,0 +1,59 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    const value1 = object1[key];
+    const value2 = object2[key];
+
+    if (typeof value1 === 'object' && typeof value2 === 'object') {
+      if (!eqObjects(value1, value2)) {
+        return false;
+      }
+    } else if (Array.isArray(value1) && Array.isArray(value2)) {
+      if (!eqArrays(value1, value2)) {
+        return false;
+      }
+    } else if (value1 !== value2) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+// Test cases
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 2, a: 1 };
+const obj3 = { a: 1, b: 2, c: 3 };
+
+assertObjectsEqual(obj1, obj2); // Objects should be equal
+assertObjectsEqual(obj1, obj3); // Objects have different lengths
