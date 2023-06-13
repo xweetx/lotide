ca5729d02f5ed1a9c860417ab1a566c20a09cc9f
xweetx <xweet_xilenze@yahoo.com>
2023-06-13 14:50:51 -0600

    eqArrays updates

--- eqArrays.js
+++ eqArrays.js
@@ -16,6 +16,5 @@ const eqArrays = function(actual, expected) {
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
